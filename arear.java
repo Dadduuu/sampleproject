9ea38558e01e88963528e7513b185a125089a60e
kirtihublikar18 <51355862+kirtihublikar18@users.noreply.github.com>
2019-06-04 23:45:48 +0530

    Add files via upload

---
+++ arear.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class arear
+{
+
+public static void main(String []b)
+{
+Scanner s=new Scanner(System.in);
+int x=s.nextInt();
+int y=s.nextInt();
+int z=x*y;
+System.out.println("The area of rectangle is "+z);
+}
+}
